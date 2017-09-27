f392c62037c5fe8cb71c9e41edbc8fe1ba1495c5
Lovjit Singh <lovjit.singh@cloudwords.com>
2017-09-27 10:12:30 +0530

    First Commit

---
+++ src/main/java/com/cw/request/fetch/FetchAssetService.java
@@ -0,0 +1,17 @@
+package com.cw.request.fetch;
+
+import com.cw.main.FetchContentIntegration;
+import com.hermease.camais.client.request.tree.FetchAssetRequest;
+import com.hermease.camais.client.response.tree.FetchAssetResponse;
+
+
+public class FetchAssetService implements FetchContentIntegration{
+	
+	public FetchAssetResponse fetchContent(FetchAssetRequest fetchAssetRequest){
+		/**
+		 * Provide your implementation here
+		 */
+		return null;
+	}
+
+}
